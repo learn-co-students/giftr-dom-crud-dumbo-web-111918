ebfe5de7d647aa79874d921e477dd6ca06c2448b
John Mark Redding <johomurk@gmail.com>
2019-01-10 17:55:26 -0500

    Finish Lab

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -1,1 +1,44 @@
-// You can add your event handlers here! 
+// You can add your event handlers here!
+function searchHandler(e) {
+  let filteredGifts = filterGifts(e.target.value, gifts);
+  let giftForm = document.querySelector("#new-gift-form");
+  if (filteredGifts.length === 0) {
+    clearEditForm()
+    showGifts(gifts);
+    giftForm.removeEventListener("submit", function () {
+      filteredGifts[0].name = document.querySelector("#gift-name-input").value;
+      filteredGifts[0].image = document.querySelector("#gift-image-input").value;
+    });
+  } else {
+    showGifts(filteredGifts);
+    fillEditForm(filteredGifts[0]);
+    giftForm.addEventListener("submit", function () {
+      filteredGifts[0].name = document.querySelector("#gift-name-input").value;
+      filteredGifts[0].image = document.querySelector("#gift-image-input").value;
+      console.log(filteredGifts[0]);
+      showGifts(filteredGifts);
+    });
+  }
+}
+
+function clearEditForm() {
+  let giftNameInput = document.querySelector("#gift-name-input");
+  let giftImageInput = document.querySelector("#gift-image-input");
+  giftNameInput.value = "";
+  giftImageInput.value = "";
+}
+function fillEditForm(gift) {
+  let giftNameInput = document.querySelector("#gift-name-input");
+  let giftImageInput = document.querySelector("#gift-image-input");
+  giftNameInput.value = gift.name;
+  giftImageInput.value = gift.image;
+}
+
+
+function removeGiftHandler(e) {
+  if (e.target.classList.contains("gift-item")) {
+    e.target.remove();
+  } else if (e.target.parentNode.classList.contains("gift-item")) {
+    e.target.parentNode.remove();
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM has been fully loaded')
-  console.table(gifts)
-})
+  let giftList = document.querySelector(".gift-list");
+  let searchInput = document.querySelector("#filter-input");
+  let giftForm = document.querySelector("#new-gift-form");
+
+  showGifts(gifts);
+  searchInput.addEventListener("input", searchHandler);
+  giftList.addEventListener("click", removeGiftHandler);
+  giftForm.addEventListener("submit", e => e.preventDefault());
+});
+
+
+function showGifts(gifts) {
+  let giftList = document.querySelector(".gift-list");
+  giftList.innerHTML = "";
+
+  gifts.forEach(function (gift) {
+    let li = document.createElement("li");
+    li.classList.add("gift-item");
+    li.innerHTML = `<img src="${gift.image}"/><h2>${gift.name}</h2>`
+    giftList.append(li);
+  });
+}
+
+function filterGifts(searchTerm, gifts) {
+  return gifts.filter(gift => (searchTerm === gift.name))
+}
